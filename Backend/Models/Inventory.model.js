cbe4351c1fd26b57c8b00f371d5872d19f0329fe
Prasad <prasadmhaske2001@gmail.com>
2023-09-20 20:36:38 +0530

    models are created

---
+++ Backend/Models/Inventory.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const InventorySchema = new Schema(
+  {
+    dealer: { type: Schema.Types.ObjectId, ref: "user", required: true },
+    oemSpecs: { type: Schema.Types.ObjectId, ref: "OEMSpecs", required: true },
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number },
+    colors: [{ type: String }],
+    kmsOnOdometer: { type: Number },
+    majorScratches: { type: String },
+    originalPaint: { type: Boolean },
+    accidentsReported: { type: Number },
+    previousBuyers: { type: Number },
+    registrationPlace: { type: String },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const InventoryModel = model("oldCar", InventorySchema);
+
+module.exports = InventoryModel;
---
+++ Backend/Models/OEM.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const OEMSchema = new Schema(
+  {
+    model: { type: String, required: true },
+    year: { type: Number, required: true },
+    listPrice: { type: Number, required: true },
+    colors: [{ type: String }],
+    mileage: { type: Number },
+    power: { type: String },
+    maxSpeed: { type: String },
+    provider: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BUser",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const OEMModel = model("OEMSpec", OEMSchema);
+
+module.exports = OEMModel;
