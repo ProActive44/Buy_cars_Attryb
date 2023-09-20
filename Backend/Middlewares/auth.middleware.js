1bab11a25f02a03d7ac461a308649ecab41574ba
Prasad <prasadmhaske2001@gmail.com>
2023-09-20 19:38:17 +0530

    userModel and auth created

---
+++ Backend/Middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).send({ msg: "Unauthorized: Token missing" });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+      if (err) {
+        console.error(err); 
+        return res.status(401).send({ msg: "Unauthorized: Token invalid" });
+      }
+      if (decoded) {
+        req.body.userId = decoded.userId;
+        next();
+      }
+    });
+  } catch (error) {
+    console.error(error); 
+    res.status(500).send({ msg: "Internal Server Error: Authorization failed" });
+  }
+};
+
+module.exports = authMiddleware;
--- Backend/Models/User.model.js
+++ Backend/Models/User.model.js
@@ -1,3 +1,14 @@
 const { Schema, model } = "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: { type: String, required },
+    email: { type: String, required },
+    password: { type: String, required },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const userModel = model("user", userSchema);
+
+module.exports = userModel;
