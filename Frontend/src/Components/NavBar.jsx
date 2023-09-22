da7b04d6c24c0c89e60d613ed2eef26aaf7dc27b
Prasad <prasadmhaske2001@gmail.com>
2023-09-23 02:14:34 +0530

    logout navigate added

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Logo from "../assets/Logo.png";
 import { Image, Box } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { LOGOUT } from "../Redux/actionTypes";
 const NavBar = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const isLogin = useSelector((store) => {
     return store.isLogin;
   });
 
   return (
-    <Box className="flex justify-between bg-blue.200 items-center h-13 border px-5 sm:px-10 bg-blue-50 rounded sticky top-0 z-10 mb-5" minW="350px">
+    <Box
+      className="flex justify-between bg-blue.200 items-center h-13 border px-5 sm:px-10 bg-blue-50 rounded sticky top-0 z-10 mb-5"
+      minW="350px"
+    >
       <div className="cursor-pointer">
         <Link to="/">
           <Image src={Logo} maxW="100px" my="1" />
@@ -24,13 +28,21 @@ const NavBar = () => {
         {!isLogin ? (
           <>
             <Link to="/sell">Sell Cars</Link>
-            <Link to='/account'>Login</Link>
-            <Link to='/account'>Sign up</Link>
+            <Link to="/account">Login</Link>
+            <Link to="/account">Sign up</Link>
           </>
         ) : (
           <>
             <Link to="/sell">SELL CARS</Link>
-            <Link onClick={() => LOGOUT(dispatch)}>LOGOUT</Link>
+            <Link
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/");
+                LOGOUT(dispatch);
+              }}
+            >
+              LOGOUT
+            </Link>
           </>
         )}
       </div>
