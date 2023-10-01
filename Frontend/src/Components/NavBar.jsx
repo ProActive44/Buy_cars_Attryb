35f24c03c3f75e388f99aee822740720d03c53d9
Prasad <prasadmhaske2001@gmail.com>
2023-10-01 18:52:32 +0530

    navbar fixed

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "../assets/Logo.png";
-import { Image, Box } from "@chakra-ui/react";
+import { Image, Box, Divider } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { LOGOUT } from "../Redux/actionTypes";
@@ -13,22 +13,22 @@ const NavBar = () => {
 
   return (
     <>
-    <Box className="text-white bg-red-500 py-2 mb-0.5 font-serif font-semibold shadow-2xl rounded-bl-3xl rounded-br-3xl">
-      Website is currently under construction, Thank you for your patience!
-    </Box>
+      <Box className="text-white text-xs md:text-base bg-red-500 py-2 mb-0.5 font-serif font-semibold shadow-2xl rounded-b-3xl">
+        Website is currently under construction, Thank you for your patience!
+      </Box>
       <Box
-        className="flex justify-between bg-blue.200 items-center h-13 border px-5 sm:px-10 bg-blue-50 rounded sticky top-0 z-10 mb-5"
+        className="flex justify-between bg-blue.200 items-center h-13 border px-1 md:px-5 sm:px-10 bg-blue-50 rounded sticky top-0 z-10 mb-5 border-b-black"
         minW="350px"
       >
         <div className="cursor-pointer">
           <Link to="/">
-            <Image src={Logo} maxW="100px" my="1" />
+            <Image src={Logo} maxW="100px" my="1" className="p-1 md:p-0" />
           </Link>
         </div>
         <div className="cursor-pointer hover:text-blue-600 hidden md:block">
           <p className="font-serif text-3xl font-extrabold">BUYC CORP</p>
         </div>
-        <div className="flex gap-2 sm:gap-5 md:gap-10 items-center mr-5">
+        <div className="flex gap-5 md:gap-10 items-center mr-5">
           {!isLogin ? (
             <>
               <Link to="/sell">Sell Cars</Link>
