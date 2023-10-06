65d382753a02d5b1a1f5753751d8fbcf306afc89
Prasad <prasadmhaske2001@gmail.com>
2023-10-06 22:06:50 +0530

    refresh to top

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,17 @@ import NavBar from "./Components/NavBar";
 import AllRoutes from "./Router/AllRoutes";
 import Loading from "./Components/Loading";
 import Footer from "./Components/Footer";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
   const isLoading = useSelector((store) => store.isLoading);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return (
     <>
